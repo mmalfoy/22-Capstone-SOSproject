69ac04b8ab4960072bf7d1af0c413ae603135e96
mmalfoy <stpmalfoy@gmail.com>
2022-08-16 06:55:44 +0900

    loading 화면 변경

--- app/src/main/java/com/example/sosproject/LoadingActivity.java
+++ app/src/main/java/com/example/sosproject/LoadingActivity.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LoadingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Glide.with(this).load(R.drawable.loading).into(image);
         startLoading();
+
+
     }
     private void startLoading() {
         Handler handler = new Handler();
